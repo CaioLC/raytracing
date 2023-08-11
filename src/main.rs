b476b6ea9228e3a13e597a0bd6d65e05d752daf4
Caio Castro <lcastro.caio@gmail.com>
2023-08-10 21:18:03 -0300

    raytracing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use std::io::{self, BufReader, BufWriter};
+use std::io::{self, BufWriter};
 use std::io::prelude::*;
 use std::fs::File;
 use glam::Vec3;
 
 const IMG_WIDTH: u32 = 1600;
 const RATIO: f32 = 16.0 / 9.0;
-const IMG_HEIGHT: u32 = (IMG_WIDTH as f32 / RATIO) as u32;
-
-const VIEWPORT_HEIGHT: f32 = 2.0;
-const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * (IMG_WIDTH as f32 / IMG_HEIGHT as f32);
 
 pub struct Ray {
     orig: Vec3,
@@ -23,18 +19,50 @@ impl Ray {
     }
 }
 
+pub fn ray_color(ray: &Ray) -> Vec3 {
+    let unit_direction = ray.dir.normalize();
+    // dbg!(unit_direction);
+    let a = 0.5*(unit_direction.y + 1.0);
+    Vec3::ONE.lerp(Vec3::new(0.5, 0.7, 1.0), a)
+}
+
 fn main() -> io::Result<()> {
-    let f = File::create("image2.ppm")?;
+    // Image
+    let img_height: u32 = 1.0_f32.max(IMG_WIDTH as f32 / RATIO) as u32;
+
+    // Camera
+    let focal_length: f32 = 1.0;
+    let viewport_height: f32 = 2.0;
+    let viewport_width: f32 = viewport_height * (IMG_WIDTH as f32 / img_height as f32);
+    let camera_center = Vec3::ZERO;
+
+    // Define the vectors across the horizontal and down the vertical viewport edges
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate horizontal and vertical delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u / IMG_WIDTH as f32;
+    let pixel_delta_v = viewport_v / img_height as f32;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left = 
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    // dbg!(viewport_upper_left);
+
+    // Open device
+    let f = File::create("image.ppm")?;
     let mut writer = BufWriter::new(f);
-    write!(&mut writer, "P3\n{IMG_WIDTH} {IMG_HEIGHT}\n255\n")?; 
-    let mut color = Vec3::ZERO;
+    write!(&mut writer, "P3\n{IMG_WIDTH} {img_height}\n255\n")?; 
+
     // Render
-    for j in 0..IMG_HEIGHT {
-        println!("Scanlines remaining: {:?}", (IMG_HEIGHT - j));
+    for j in 0..img_height {
+        println!("Scanlines remaining: {:?}", (img_height - j));
         for i in 0..IMG_WIDTH {
-            color.x = i as f32 / IMG_WIDTH as f32;
-            color.y = j as f32 / IMG_HEIGHT as f32;
-            color.z = 0.0;
+            let pixel_center = pixel00_loc + (i as f32 *pixel_delta_u) + (j as f32 * pixel_delta_v);
+            let ray_dir = pixel_center - camera_center;
+            let ray = Ray::new(camera_center, ray_dir);
+            let color = ray_color(&ray);
             write_color(&mut writer, &color)?;
         }
     };
